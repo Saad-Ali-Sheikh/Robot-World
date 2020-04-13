2c942e662f5e9d23307791ca057f7f3e385c7f56
unknown <saadali.sheikh7865@example.com>
2020-04-13 20:15:23 +0500

    Updates

---
+++ precache-manifest.d14779cdbc14b13c0e4746f4188a17d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2d445da3001b6bbb67ed149832c120",
+    "url": "/Robot-World/index.html"
+  },
+  {
+    "revision": "265ae7a5fbee32056e0b",
+    "url": "/Robot-World/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "c14c9ad3afb043809191",
+    "url": "/Robot-World/static/css/main.7851f0d7.chunk.css"
+  },
+  {
+    "revision": "265ae7a5fbee32056e0b",
+    "url": "/Robot-World/static/js/2.21937ac1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robot-World/static/js/2.21937ac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14c9ad3afb043809191",
+    "url": "/Robot-World/static/js/main.f4beab26.chunk.js"
+  },
+  {
+    "revision": "7ab08c4b9b0407c2a799",
+    "url": "/Robot-World/static/js/runtime-main.2baa62a2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robot-World/static/media/SEGA.12a3db8b.woff"
+  }
+]);
